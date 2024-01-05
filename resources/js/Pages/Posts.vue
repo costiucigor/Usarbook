54833c317485af2c86c25a923ddc10f7e702c68b
costiucigor <gast777@yahoo.com>
2024-01-05 16:35:02 +0200

    project setup

---
+++ resources/js/Pages/Posts.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <Head title="Posts" />
+
+    <MainNavLayout>
+        Posts
+    </MainNavLayout>
+</template>
